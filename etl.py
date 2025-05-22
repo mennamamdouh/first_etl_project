9d2fca7072c75dc12630390d15b641cf107c1ae5
mennamamdouh <es-menna.mamdouh1701@alexu.edu.eg>
2025-05-22 11:47:54 +0300

    added new line

--- etl.py
+++ etl.py
@@ -15,4 +15,4 @@ df["purchasedate"] = pd.to_datetime(df["purchasedate"], errors="coerce")
 # Save the final processed data into a local file
 df.to_csv("final_data.csv", index=False)
 
-# End of etl pipeline
+# End of etl pipeline# End of ETL pipeline
